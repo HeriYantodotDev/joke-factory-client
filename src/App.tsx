3a2646ffc31d37391461a70a58ff0f2e85ae7620
HeriYantodotDev <yanto.heri@outlook.com>
2023-07-26 14:01:10 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from './pages/SignUp/SignUp.component';
+
+function App() {
+  return (
+    <div className='mx-7 mt-10 mb-10'>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
